93e2c9c0777297595de62af34e10330ddc1d405f
Christian Nainggolan <christiannainggolan2755@gmail.com>
2024-07-01 16:26:03 +0700

    feat:add register validation

---
+++ restful-ts/src/service/user-service.ts
@@ -0,0 +1,6 @@
+import { CreateUserRequest } from "../model/user-model";
+
+export class UserService {
+    static async register(request:CreateUserRequest) : Promise<UserRespone> {
+        
+    }
