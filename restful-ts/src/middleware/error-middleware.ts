031b64e804fb03fd4f8d2db525663211fed52823
Christian Nainggolan <christiannainggolan2755@gmail.com>
2024-07-02 21:41:07 +0700

    feat:add error500 middleware

--- restful-ts/src/middleware/error-middleware.ts
+++ restful-ts/src/middleware/error-middleware.ts
@@ -7,5 +7,7 @@ export const errorMiddleware = async (error: Error, req: Request, res: Response,
     res.status(400).json({ errors: `Validation Error : ${JSON.stringify(error)}` });
   } else if (error instanceof ResponseError) {
     res.status(error.status).json({ errors: error.message });
+  } else {
+    res.status(500).json({ errors: error.message });
   }
 };
