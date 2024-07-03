a2cb429d77ee281fb418c0c48dfbbea674844e4a
Christian Nainggolan <christiannainggolan2755@gmail.com>
2024-07-03 22:08:50 +0700

    feat(test):add test-util

--- restful-ts/test/user.test.ts
+++ restful-ts/test/user.test.ts
@@ -1,20 +1,25 @@
 import supertest from 'supertest';
 import { web } from '../src/application/web';
 import { logger } from '../src/application/logging';
+import { UserTest } from './test-util';
 
 describe('POST /api/users', () => {
-  it('should reject register new user if request is invalid', async () => {
-    const response = await supertest(web).post('/api/users').send({
-      username: '',
-      password: '',
-      name: '',
-    });
+  // afterEach(async () => {
+  //   await UserTest.delete();
+  // });
 
-    logger.debug(response.body);
+  // it('should reject register new user if request is invalid', async () => {
+  //   const response = await supertest(web).post('/api/users').send({
+  //     username: '',
+  //     password: '',
+  //     name: '',
+  //   });
 
-    expect(response.status).toBe(400);
-    expect(response.body.errors).toBeDefined();
-  });
+  //   logger.debug(response.body);
+
+  //   expect(response.status).toBe(400);
+  //   expect(response.body.errors).toBeDefined();
+  // });
 
   it('should register new user', async () => {
     const response = await supertest(web).post('/api/users').send({
@@ -26,7 +31,7 @@ describe('POST /api/users', () => {
     logger.debug(response.body);
 
     expect(response.status).toBe(200);
-    expect(response.body.data.username).toBe('christian');
-    expect(response.body.data.name).toBe('Christian Morgan');
+    expect(response.body.data.username).toBe('test');
+    expect(response.body.data.name).toBe('test');
   });
 });
